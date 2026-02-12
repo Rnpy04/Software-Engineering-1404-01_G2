9df37463f59d89a5c6b9d5f3e349f1251fba0429
Amyr-333 <mate27477@gmail.com>
2026-02-12 19:54:02 +0330

    Add Map Service implementation (Interactive Map + Facilities API)

--- team9/urls.py
+++ team9/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.base),
     path("ping/", views.ping),
+    path("map/", views.map_view),
+    path("api/mock-facilities/", views.mock_facilities_api),
 ]
